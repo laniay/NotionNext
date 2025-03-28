8219305231e47e6a09a5191cc1ab95587825df01
laniay <126132407+laniay@users.noreply.github.com>
2025-03-28 19:54:51 +0900

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,9 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://www.notion.com/'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        Notion {siteConfig('VERSION')}
       </a>
       .
     </div>
